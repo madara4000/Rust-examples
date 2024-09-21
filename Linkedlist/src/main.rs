6ba2a82b55d989f0385abb600b264f676f09ab6f
madara4000 <yagomartins4000@gmail.com>
2024-09-21 20:55:37 -0300

    list inicio implrmentação

--- Linkedlist/src/main.rs
+++ Linkedlist/src/main.rs
@@ -1,8 +1,30 @@
+struct Pessoa {
+    nome: String,
+    idade: i32,
+    telefone:i32
+}
 struct Node{
-    int data;
-    box<
+    dado: Pessoa,
+    proximo: Option<Box<Node>>
 
 }
+struct List
+{head:Option<Box<Node>>
+
+}
+impl List {
+    fn List_Search(&self,k:i32)-> Option<&Node>  {
+    let mut now= &self.head;
+    while  let Some(node) =now {
+      if node.dado.idade == k{
+        return Some(node);
+      }
+     now= &node.proximo;
+    }
+    None
+}
+}
+
 fn main() {
     println!("Hello, world!");
 }
